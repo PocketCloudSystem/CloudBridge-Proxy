8c8a4c39f12ec2e781d9900f4e6348d016e63288
r3pt1s <leonistmegacool2@gmail.com>
2026-04-29 16:51:51 +0200

    performance boost during server start up

--- src/main/java/de/pocketcloud/cloud/bridge/task/RequestTimeoutTask.java
+++ src/main/java/de/pocketcloud/cloud/bridge/task/RequestTimeoutTask.java
@@ -1,7 +1,9 @@
 package de.pocketcloud.cloud.bridge.task;
 
 import de.pocketcloud.cloud.bridge.network.packet.RequestPacket;
+import de.pocketcloud.cloud.bridge.network.packet.impl.request.ServerHandshakeRequestPacket;
 import de.pocketcloud.cloud.bridge.network.request.RequestManager;
+import de.pocketcloud.cloud.bridge.util.CloudEnvironmentConfig;
 import de.pocketcloud.cloud.bridge.util.Utils;
 import dev.waterdog.waterdogpe.scheduler.Task;
 
@@ -10,7 +12,8 @@ final public class RequestTimeoutTask extends Task {
     @Override
     public void onRun(int i) {
         for (RequestPacket packet : RequestManager.getInstance().getAll().values()) {
-            if ((packet.getSentTimestamp() + 10) < Utils.time()) {
+            int timeout = packet instanceof ServerHandshakeRequestPacket ? CloudEnvironmentConfig.getServerTimeout() : 10;
+            if ((packet.getSentTimestamp() + timeout) < Utils.time()) {
                 RequestManager.getInstance().reject(packet);
                 RequestManager.getInstance().remove(packet);
             }
